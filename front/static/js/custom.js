73f70fefd3b2f5683d06d689935fc981386dea35
aashish <aashish2024@outlook.com>
2024-07-31 18:58:05 +0530

    v1

---
+++ front/static/js/custom.js
@@ -0,0 +1,11 @@
+function sweet(){
+    Swal.fire({
+        title: 'About',
+        confirmButtonText: 'Close',
+        html:
+        'This is a project by <a href="https://github.com/farthestmage"><u>farthestmage</u></a> & <a href="https://github.com/pseudowudo"><u>pseudowudo</u><a>. The aim is to provide a dashboard-like experience to the user where they can check out various essential information and features like weather, IP, to-do list, etc.',
+
+
+      })
+}
+
